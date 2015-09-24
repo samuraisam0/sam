b7443416c229ff7223971549068b8672a72fa1cc
test@test.com <test@test.com>
2015-09-24 21:06:30 +0200

    first commit

---
+++ src/genesis.h
@@ -0,0 +1,13 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("0x00000faae0d547cbfb311f4145e81cc8dd61ab5ad9a6c3b3d2f92582f1341961");
+static const uint256 hashGenesisBlockTestNet("");
+
+
+
+#endif // GENESIS_H
